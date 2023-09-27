e92a6063e4844f54bdaf89cf153dfb07bff30d96
Bejka1 <145840115+Bejka1@users.noreply.github.com>
2023-09-27 08:50:14 +0200

    Create prva hodina.py

---
+++ prva hodina.py
@@ -0,0 +1,17 @@
+#moj prvy program
+print ("vitaj")
+print ()
+print ("vypocet je",3 + 5 * 5)
+print ("ahoj" + " "+ "ako")
+print (" vypocet je",9 - 12 + 457)
+print ("vysledok je",987 * 48)
+print ("vysledok je",93 - 54)
+
+#bude sa cakat na stlacenie klavesu ENTER
+a=int (input ("napis dake cislo"))
+b= 5
+c= a+b
+print(c)
+
+
+
