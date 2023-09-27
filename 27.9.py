73798bb366cbc8ebd3b8d36432793ab019309d2e
Bejka1 <145840115+Bejka1@users.noreply.github.com>
2023-09-27 10:30:09 +0200

    Create 27.9.py

---
+++ 27.9.py
@@ -0,0 +1,24 @@
+x= int(input("Zadaj cislo ktore checs nasobit *6:"))
+print("Vysledok je:", x*6)
+meno = input("ako sa volas?")
+print("ahoj", meno)
+
+roky = input ("kolko mas rokov")
+print("mam", roky)
+
+farba = input ("aka je tvoja oblubena farba")
+print("moja oblubena farba je", farba)
+
+vysledok = input ("kolko je 5 * 5")
+print("vysledok je", vysledok)
+
+vaha = input ("kolko vazis")
+print("vazim", vaha)
+
+vysledok = input ("kolko je 24 - 6")
+print("vysledok je", vysledok)
+
+x= int(input("Zadaj cislo ktore checs nasobit *6:"))
+print("Vysledok je:", x*6)
+
+ 
