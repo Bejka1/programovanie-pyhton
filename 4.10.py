f77d771df6f3c224b70d517b2342bed0a1c294d0
Bejka1 <145840115+Bejka1@users.noreply.github.com>
2023-10-04 10:04:50 +0200

    Add files via upload

---
+++ 4.10.py
@@ -0,0 +1,16 @@
+for i in 1,2,3,4,5:
+    print("Programujem v pythone")
+
+for x in 5,7,11,13,23:
+    x2=x**2
+    print("druha mocnina", x , "je", x2)
+
+for x in 1,2,3,4:
+    x5=x**5
+    print("piata mocnina", x , "je", x5)
+
+for pismeno in "Python":
+    print(pismeno*10)
+
+
+    
