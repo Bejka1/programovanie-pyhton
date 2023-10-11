dc5f37e13c3945f19f625f5e0249cc16385b9e55
Bejka1 <145840115+Bejka1@users.noreply.github.com>
2023-10-11 10:02:03 +0200

    Add files via upload

---
+++ 11.10.py
@@ -0,0 +1,19 @@
+x =int(input("zadaj cislo: "))
+for i in range(1,11):
+    print(x*i, end=" ")
+
+a=int(input("zadaj cislo: "))
+for b in range(1,101):
+    print(a*b, end=" ")
+
+z=int(input("zadaj cislo: "))
+print(z**2)
+print()
+
+d=int(input("zadaj cislo ktore chces nasobit 4: "))
+print(d*4)
+
+      
+    
+
+     
