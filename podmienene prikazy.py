fa00c46912b5a37df689bd41a96fc6f353aac5f0
Bejka1 <145840115+Bejka1@users.noreply.github.com>
2023-11-08 09:10:43 +0100

    Add files via upload

---
+++ podmienene prikazy.py
@@ -0,0 +1,15 @@
+body= int (input("Zadaj kolko bodov si ziskal v teste: "))
+if body >=90:
+    print("za", body, "ziskas znamku A")
+
+elif body >=80:
+    print ("za", body, "ziskas znamku B")
+elif body >=70:
+    print ("za", body, "ziskas znamku C")
+elif body >=60:
+    print ("za", body, "ziskas znamku D")
+elif body >=50:
+    print ("za", body, "ziskas znamku E")
+    
+else:
+    print ("za", body, "dostavas FENIXA")
